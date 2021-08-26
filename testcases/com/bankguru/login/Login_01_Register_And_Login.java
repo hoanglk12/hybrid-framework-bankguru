e21c05064bc5f51ea12cde99f6e0147c6b8d4495
Hoang Pham <hoanglk12@gmail.com>
2021-08-26 23:50:48 +0700

    Init first automation hybrid framework

---
+++ testcases/com/bankguru/login/Login_01_Register_And_Login.java
@@ -0,0 +1,19 @@
+package com.bankguru.login;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Login_01_Register_And_Login {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void New_Customer_Validate_Name() {
+	}
+	
+	@AfterClass
+	public void afterClass() {
+	}
+}
